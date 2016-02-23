c8582364056ac9af0a012e85fbc7e73b8cd294a9
Lisa Zhang <amphithere.19@gmail.com>
2016-02-23 12:51:22 -0700

    Trying to get mailer working

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,7 +74,18 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = { :host => 'researchmapproject.herokuapp.com' }
+  config.action_mailer.default_url_options = {:host => 'researchmapproject.herokuapp.com'}
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            ENV["email"],
+    password:             ENV["password"],
+    authentication:       'plain',
+    enable_starttls_auto: true  }
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
