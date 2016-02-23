e3f0e26f557b8d699479e4df326e51e2fa4ed0e5
Lisa Zhang <amphithere.19@gmail.com>
2016-02-23 12:29:17 -0700

    Modified mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
   config.assets.raise_runtime_errors = true
 
   # For devise
-  config.action_mailer.default_url_options = {:host => 'localhost:3000'}
+  config.action_mailer.default_url_options = {:host => 'researchmapproject.herokuapp.com'}
   config.action_mailer.perform_deliveries = true
 
   config.action_mailer.delivery_method = :smtp
