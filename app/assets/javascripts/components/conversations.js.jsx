54cbcf7c1e8ab4fb3885b37c9d1f151baa268044
Lisa Zhang <amphithere.19@gmail.com>
2016-05-04 20:54:24 -0700

    Moving components in react

--- app/assets/javascripts/components/conversations.js.jsx
+++ app/assets/javascripts/components/conversations.js.jsx
@@ -44,6 +44,33 @@ var MessageList = React.createClass({
     }
 });
 
+var Reply = React.createClass({
+    getInitialState: function(){
+      return {text: ''};
+    },
+    handleTextChange: function(e){
+      this.setState({text: e.target.value});
+    },
+    handleSubmit: function(e){
+      e.preventDefault();
+      var text = this.state.text.trim();
+      if (!text){
+        return;
+      }
+      this.props.onReplySubmit(text);
+      this.setState({text:''});
+    },
+    render: function(){
+    return(
+      <div className='form-group'>
+        <form className = "replyForm" onSubmit = {this.handleSubmit}>
+          <textarea rows = '4' required = "true" cols='50' className = 'form-control replyBody' type = 'text' id = "body" name = "body" placeholder = "Reply..." value = {this.state.text} onChange = {this.handleTextChange}/>
+          <button className = 'btn btn-default' type = 'submit'>{this.props.button}</button>
+        </form>
+      </div>
+    )
+  }
+});
 
 var Conversation = React.createClass({
   getInitialState: function (){
@@ -101,36 +128,6 @@ var Conversation = React.createClass({
           </div>
         </div>
           <Reply button = {this.state.button} onReplySubmit = {this.handleMessageSubmit}/>
-      </div>
-    )
-  }
-});
-
-
-
-var Reply = React.createClass({
-    getInitialState: function(){
-      return {text: ''};
-    },
-    handleTextChange: function(e){
-      this.setState({text: e.target.value});
-    },
-    handleSubmit: function(e){
-      e.preventDefault();
-      var text = this.state.text.trim();
-      if (!text){
-        return;
-      }
-      this.props.onReplySubmit(text);
-      this.setState({text:''});
-    },
-    render: function(){
-    return(
-      <div className='form-group'>
-        <form className = "replyForm" onSubmit = {this.handleSubmit}>
-          <textarea rows = '4' required = "true" cols='50' className = 'form-control replyBody' type = 'text' id = "body" name = "body" placeholder = "Reply..." value = {this.state.text} onChange = {this.handleTextChange}/>
-          <button className = 'btn btn-default' type = 'submit'>{this.props.button}</button>
-        </form>
       </div>
     )
   }
