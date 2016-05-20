b6c070876825ed5488c7018c149e492651be5524
Lisa Zhang <amphithere.19@gmail.com>
2016-05-20 10:21:39 -0700

    Added a Researcher Sign-up button to the nagivation bar

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Role.create!(name: :"Regular User", description: :"Able to browse Research and Researchers, can message Researchers, like Research and follow Researchers.")
-Role.create!(name: :"Patron", description: :"All abilities of Regular User, but may request a Trip Pass of 1 day.")
-Role.create!(name: :"Benefactor", description: :"All the abilities of a Regular User, but may request Trip Passes up to 7 days.")
-Role.create!(name: :"Administrator", description: :"All abilities of a Regular User, but may import Users, Researchers, Research, Trip Passes and has administrative rights to manage Users, Researchers, Research and Trip Passes.")
+Role.create!(id: 1, name: :"Regular User", description: :"Able to browse Research and Researchers, can message Researchers, like Research and follow Researchers.")
+Role.create!(id: 2, name: :"Patron", description: :"All abilities of Regular User, but may request a Trip Pass of 1 day.")
+Role.create!(id: 3, name: :"Benefactor", description: :"All the abilities of a Regular User, but may request Trip Passes up to 7 days.")
+Role.create!(id: 4, name: :"Admin", description: :"All abilities of a Regular User, but may import Users, Researchers, Research, Trip Passes and has administrative rights to manage Users, Researchers, Research and Trip Passes.")
